0e9c7a41cdd7f9feb63987bffab32bd855f1c443
Adityashahi4465 <97585273+Adityashahi4465@users.noreply.github.com>
2022-07-21 01:43:29 +0530

    added structures and file_hendling

---
+++ file_handlin_entry.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main()
+{
+
+    char name[50];
+    char rno[10];
+    float marks;
+    int no;
+
+    printf("No. of students (of whom data is going to be entered): ");
+    scanf("%d", &no);
+
+    FILE *fp;
+
+    fp = fopen("Data.txt", "a");
+
+    for (int i = 1; i < no + 1; i++)
+    {
+        fflush(stdin);
+        printf("\nRecord number: %d\n", i);
+
+        printf("Student Roll no.: ");
+        scanf("%s", rno);
+
+        printf("Student name: ");
+        scanf("%s", name);
+
+        printf("Student marks: ");
+        scanf("%f", &marks);
+
+        fprintf(fp, "%s,%s,%f\n", rno, name, marks);
+        fflush(stdin);
+    }
+    fclose(fp);
+
+    printf("\n\n--------Reading Data from the records--------\n\n");
+
+    fp = fopen("Data.txt", "r");
+
+    char c;
+    c = getc(fp);
+
+    while (c != EOF)
+    {
+        printf("\t%c", c);
+        c = getc(fp);
+    }
+
+    printf("\n COMPLETED!");
+
+    return 0;
+}
